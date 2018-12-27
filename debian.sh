ddf2dab8a0ae861fac5fa527347c241982f3a325
gbzhang <gbzhang@outlook.com>
2018-12-27 10:57:23 +0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -5,6 +5,13 @@ apt-get install git -y
 git clone https://github.com/gbzhang/config
 cd config
 cp sources.list /etc/apt/sources.list
+wget https://mirrors.ustc.edu.cn/deb-multimedia/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
+dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
+wget https://repo.debiancn.org/pool/main/d/debiancn-keyring/debiancn-keyring_0~20161212_all.deb -O /tmp/debiancn-keyring.deb
+apt install /tmp/debiancn-keyring.deb
+apt update
+rm /tmp/debiancn-keyring.deb
+rpm deb-multimedia-keyring_2016.8.1_all.deb
 aptitude update
 aptitude dist-upgrade -y
 cp virtualbox.list /etc/apt/sources.list.d/virtualbox.list
