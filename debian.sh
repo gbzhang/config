3da9a1341dd86e3af33c94ea5da1808d57bd4d74
gbzhang <gbzhang@outlook.com>
2018-12-27 11:51:17 +0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -7,14 +7,13 @@ apt-get install apt-transport-https -y
 cp sources.list /etc/apt/sources.list
 wget https://mirrors.ustc.edu.cn/deb-multimedia/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
 dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
-wget https://repo.debiancn.org/pool/main/d/debiancn-keyring/debiancn-keyring_0~20161212_all.deb -O /tmp/debiancn-keyring.deb
-apt install /tmp/debiancn-keyring.deb
-apt update
-rm /tmp/debiancn-keyring.deb
-rpm deb-multimedia-keyring_2016.8.1_all.deb
+wget http://mirrors.ustc.edu.cn/debiancn/pool/main/d/debiancn-keyring/debiancn-keyring_0~20161212_all.deb
+dpkg -i debiancn-keyring_0~20161212_all.deb
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
 aptitude update
+rm debiancn-keyring_0~20161212_all.deb
+rm deb-multimedia-keyring_2016.8.1_all.deb
 aptitude dist-upgrade -y
 cp virtualbox.list /etc/apt/sources.list.d/virtualbox.list
 aptitude -y install octave grace kile p4vasp vim lyx gelemental \
