85543f2c8bcd16d2954175fbeae053e31836c8ca
gbzhang <gbzhang@outlook.com>
2018-12-27 10:49:03 +0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-apt-get install aptitude
-apt-get install apt-transport-https
-apt-get install git
-git clone https://github.com/gbzhang/sources.list
+apt-get install aptitude -y
+apt-get install apt-transport-https -y
+apt-get install git -y
+git clone https://github.com/gbzhang/config
+cd config
 cp sources.list /etc/apt/sources.list
 aptitude update
 aptitude dist-upgrade -y
-git clone https://github.com/gbzhang/virtualbox.list
 cp virtualbox.list /etc/apt/sources.list.d/virtualbox.list
-aptitude install octave grace kile p4vasp vim lyx gelemental \
+aptitude -y install octave grace kile p4vasp vim lyx gelemental \
   xcrysden texlive-science texlive-publishers \
   sudo wxmaxima inkscape fonts-wqy-microhei fonts-wqy-zenhei \
   ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy 
-aptitude install virtulbox
+aptitude install virtulbox -y
