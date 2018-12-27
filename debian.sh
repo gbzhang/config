2979a43f96597900a0892e879c0f681985000160
gbzhang <gbzhang@outlook.com>
2018-12-27 11:18:00 +0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt-get install aptitude -y
 apt-get install apt-transport-https -y
-apt-get install git -y
-git clone https://github.com/gbzhang/config
-cd config
+#apt-get install git -y
+#git clone https://github.com/gbzhang/config
+#cd config
 cp sources.list /etc/apt/sources.list
 wget https://mirrors.ustc.edu.cn/deb-multimedia/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
 dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
@@ -12,6 +12,8 @@ apt install /tmp/debiancn-keyring.deb
 apt update
 rm /tmp/debiancn-keyring.deb
 rpm deb-multimedia-keyring_2016.8.1_all.deb
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
 aptitude update
 aptitude dist-upgrade -y
 cp virtualbox.list /etc/apt/sources.list.d/virtualbox.list
@@ -19,4 +21,4 @@ aptitude -y install octave grace kile p4vasp vim lyx gelemental \
   xcrysden texlive-science texlive-publishers \
   sudo wxmaxima inkscape fonts-wqy-microhei fonts-wqy-zenhei \
   ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy 
-aptitude install virtulbox -y
+aptitude install virtulbox-6.0 -y
