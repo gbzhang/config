1777a3206cd7e1207945a868b76dd7c0dbeae0d8
gbzhang <gbzhang@outlook.com>
2019-03-06 20:19:27 +0800

    Add files via upload

---
+++ synclient.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+synclient TapButton1=1 TapButton2=3 TapButton3=2  
+synclient PalmDetect=1  # 开启掌压感应
+synclient PalmMinWidth=8 # 设定手掌与触摸板的最小接触面,
+#i超过该值认为触摸板被手掌压到了(单位是手指宽度?)
+#synclient PalmMinZ=10 
+
